151f2c643499ce542067cf926fbc112739c9b547
Max <clashshow2000@gmail.com>
2025-04-17 17:59:37 +0500

    first commit

---
+++ src/main/java/com/maxlvshv/foodsharingback/FoodSharingBackApplication.java
@@ -0,0 +1,13 @@
+package com.maxlvshv.foodsharingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSharingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSharingBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxlvshv/foodsharingback/FoodSharingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxlvshv.foodsharingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSharingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
